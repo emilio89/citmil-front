8518801549e9aa3090edc64857c62bdc97f20fba
Emilio <emilio89@gmail.com>
2020-05-13 18:52:56 +0200

    cambio para tener el color del nav-bar en una variable, primeramente habría que quitar el bg-primary

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { JhiLanguageService } from 'ng-jhipster';
-import { SessionStorageService } from 'ngx-webstorage';
+import { Component, OnInit } from "@angular/core"
+import { Router } from "@angular/router"
+import { JhiLanguageService } from "ng-jhipster"
+import { SessionStorageService } from "ngx-webstorage"
 
-import { VERSION } from 'app/app.constants';
-import { LANGUAGES } from 'app/core/language/language.constants';
-import { AccountService } from 'app/core/auth/account.service';
-import { LoginModalService } from 'app/core/login/login-modal.service';
-import { LoginService } from 'app/core/login/login.service';
-import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { VERSION } from "app/app.constants"
+import { LANGUAGES } from "app/core/language/language.constants"
+import { AccountService } from "app/core/auth/account.service"
+import { LoginModalService } from "app/core/login/login-modal.service"
+import { LoginService } from "app/core/login/login.service"
+import { ProfileService } from "app/layouts/profiles/profile.service"
 
 @Component({
-  selector: 'jhi-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['navbar.scss']
+  selector: "jhi-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["navbar.scss"]
 })
 export class NavbarComponent implements OnInit {
-  inProduction?: boolean;
-  isNavbarCollapsed = true;
-  languages = LANGUAGES;
-  swaggerEnabled?: boolean;
-  version: string;
+  inProduction?: boolean
+  isNavbarCollapsed = true
+  languages = LANGUAGES
+  swaggerEnabled?: boolean
+  version: string
+  color = "yellow"
 
   constructor(
     private loginService: LoginService,
@@ -31,44 +32,51 @@ export class NavbarComponent implements OnInit {
     private profileService: ProfileService,
     private router: Router
   ) {
-    this.version = VERSION ? (VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION) : '';
+    this.version = VERSION ? (VERSION.toLowerCase().startsWith("v") ? VERSION : "v" + VERSION) : ""
   }
 
   ngOnInit(): void {
     this.profileService.getProfileInfo().subscribe(profileInfo => {
-      this.inProduction = profileInfo.inProduction;
-      this.swaggerEnabled = profileInfo.swaggerEnabled;
-    });
+      this.inProduction = profileInfo.inProduction
+      this.swaggerEnabled = profileInfo.swaggerEnabled
+    })
   }
 
   changeLanguage(languageKey: string): void {
-    this.sessionStorage.store('locale', languageKey);
-    this.languageService.changeLanguage(languageKey);
+    this.sessionStorage.store("locale", languageKey)
+    this.languageService.changeLanguage(languageKey)
   }
 
   collapseNavbar(): void {
-    this.isNavbarCollapsed = true;
+    this.isNavbarCollapsed = true
   }
 
   isAuthenticated(): boolean {
-    return this.accountService.isAuthenticated();
+    return this.accountService.isAuthenticated()
   }
 
   login(): void {
-    this.loginModalService.open();
+    this.loginModalService.open()
   }
 
   logout(): void {
-    this.collapseNavbar();
-    this.loginService.logout();
-    this.router.navigate(['']);
+    this.collapseNavbar()
+    this.loginService.logout()
+    this.router.navigate([""])
   }
 
   toggleNavbar(): void {
-    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+    this.isNavbarCollapsed = !this.isNavbarCollapsed
   }
 
   getImageUrl(): string {
-    return this.isAuthenticated() ? this.accountService.getImageUrl() : '';
+    return this.isAuthenticated() ? this.accountService.getImageUrl() : ""
+  }
+
+  setMyStyles(): any {
+    const styles = {
+      "background-color": this.color + "!important"
+    }
+    return styles
   }
 }
